6d09f674cf5e1db3e7b0191c3d6fb91ea6a56e79
Atul Varma <varmaa@gmail.com>
2014-03-13 17:48:03 -0400

    added /t/:tag endpoint.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+var STATIC_DIR = __dirname + '/static';
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(STATIC_DIR));
+
+app.get('/t/:tag', function(req, res) {
+  return fs.createReadStream(STATIC_DIR + '/tag-based-minicade.html')
+    .pipe(res);
+});
 
 app.listen(PORT, function() {
   console.log('Listening on port', PORT);

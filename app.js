4d0586c0ea48c1b417f76247a1c432f03645edff
Atul Varma <varmaa@gmail.com>
2014-03-14 16:54:31 -0400

    added base CSS sheet in LESS.

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-var fs = require('fs');
 var express = require('express');
-var mustache = require('mustache');
-var app = express();
 
 var PORT = process.env.PORT || 3000;
 var DEBUG = 'DEBUG' in process.env;
 var STATIC_DIR = __dirname + '/static';
 var TAG_RE = /^([A-Za-z0-9_\-]+)$/;
 
-var templateCache = {};
-
-function loadTemplate(name) {
-  var template = fs.readFileSync(STATIC_DIR + '/' + name, 'utf-8');
-  mustache.parse(template);
-  return template;
-}
-
-function renderTemplate(name, ctx) {
-  if (!(name in templateCache) || DEBUG)
-    templateCache[name] = loadTemplate(name);
-  return mustache.render(templateCache[name], ctx);
-}
+var app = express();
+var renderTemplate = require('./lib/render-template')({
+  rootDir: STATIC_DIR,
+  debug: DEBUG
+});
+var renderLess = require('./lib/render-less')({
+  paths: [STATIC_DIR + '/css'],
+  debug: DEBUG
+});
 
 app.use(express.static(STATIC_DIR));
 
+app.get('/css/base.css', renderLess('base.less'));
+
 app.get('/t/:tag', function(req, res, next) {
   if (!TAG_RE.test(req.params.tag)) return next();
-  return res.send(renderTemplate('tag-based-minicade.html', {
+  renderTemplate(res, 'tag-based-minicade.html', {
     tag: req.params.tag
-  }));
+  });
 });
 
 app.listen(PORT, function() {

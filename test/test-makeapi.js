cd2e619a41bc7837483b9601f5133b226569100d
Atul Varma <varmaa@gmail.com>
2014-03-17 08:44:14 -0400

    added lib/makeapi.js with nock-based tests.

---
+++ test/test-makeapi.js
@@ -0,0 +1,45 @@
+var nock = require('nock');
+var should = require('should');
+
+var makeapi = require('../lib/makeapi');
+
+describe('makeapi.doesTagExist()', function() {
+  var mockedMake;
+
+  beforeEach(function() {
+    mockedMake = nock('https://makeapi.webmaker.org');
+  });
+
+  afterEach(function() {
+    mockedMake.done();
+  });
+
+  it('should return true when makes w/ tag exist', function(done) {
+    mockedMake.get('/api/20130724/make/search?tags=exists&limit=1')
+      .reply(200, {makes: [{this_is: 'a fake make'}]});
+    makeapi.doesTagExist('exists', function(err, exists) {
+      if (err) return done(err);
+      exists.should.be.true;
+      done();
+    });
+  });
+
+  it('should return false when makes w/ tag do not exist', function(done) {
+    mockedMake.get('/api/20130724/make/search?tags=exists&limit=1')
+      .reply(200, {makes: []});
+    makeapi.doesTagExist('exists', function(err, exists) {
+      if (err) return done(err);
+      exists.should.be.false;
+      done();
+    });
+  });
+
+  it('should return an err when status is not 200', function(done) {
+    mockedMake.get('/api/20130724/make/search?tags=exists&limit=1')
+      .reply(404);
+    makeapi.doesTagExist('exists', function(err, exists) {
+      err.message.should.eql('got status code 404');
+      done();
+    });
+  });
+});

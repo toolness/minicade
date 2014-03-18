3d480b4815fb3694f2149d8010f382ad308a772d
Atul Varma <varmaa@gmail.com>
2014-03-18 17:26:14 -0400

    added GistCache and tests. working towards #6.

---
+++ test/test-gist-cache.js
@@ -0,0 +1,60 @@
+var should = require('should');
+var nock = require('nock');
+
+var GistCache = require('../lib/gist-cache');
+
+describe('GistCache', function() {
+  it('should return cached entries when possible', function(done) {
+    var gists = new GistCache();
+    var github = nock('https://api.github.com')
+      .get('/gists/12345')
+      .reply(200, {data: 'awesome'}, {'ETag': '"here is an etag"'})
+      .get('/gists/12345')
+      .matchHeader('if-none-match', '"here is an etag"')
+      .reply(304)
+      .get('/gists/12345')
+      .matchHeader('if-none-match', '"here is an etag"')
+      .reply(304);
+
+    gists.get(12345, function(err, gist) {
+      if (err) return done(err);
+
+      gist.should.eql({data: 'awesome'});
+      gists.get(12345, function(err, gist) {
+        if (err) return done(err);
+        gist.should.eql({data: 'awesome'});
+
+        gists.get(12345, function(err, gist) {
+          if (err) return done(err);
+          gist.should.eql({data: 'awesome'});
+
+          github.done();
+          done();
+        });
+      });
+    });
+  });
+
+  it('should return status', function(done) {
+    var gists = new GistCache();
+    var github = nock('https://api.github.com')
+      .get('/rate_limit')
+      .reply(200, {some: 'info'});
+
+    gists.status(function(err, info) {
+      if (err) return done(err);
+      info.should.eql({
+        cache: {
+          itemCount: 0,
+          length: 0
+        },
+        rateLimit: {
+          some: 'info'
+        }
+      });
+
+      github.done();
+      done();
+    });
+  });
+});

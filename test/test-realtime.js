07f086d9c378fbbd993fc5d1ecaaea2a6f1c8a6e
Atul Varma <varmaa@gmail.com>
2014-12-13 13:45:54 -0500

    realtime tests now use realtime client.

--- test/test-realtime.js
+++ test/test-realtime.js
@@ -1,15 +1,24 @@
 var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore');
 var should = require('should');
 
 var storages = require('../lib/storages');
 var realtime = require('../lib/realtime');
 
+function noop() {
+}
+
+function withoutGuids(games) {
+  return games.map(function(game) {
+    return _.omit(game, 'id');
+  });
+}
+
 function FakeWs() {
   var self = new EventEmitter();
 
-  self._sent = [];
   self.send = function(data) {
-    self._sent.push(JSON.parse(data));
+    self.emit('_send', data);
   };
 
   return self;
@@ -25,16 +34,33 @@ describe('realtime', function() {
 
   it('should work', function(done) {
     var ws = FakeWs();
+    var client = realtime.Client(function sendMessage(data) {
+      //console.log("SEND", data);
+      ws.emit('message', JSON.stringify(data));
+    }, noop);
+    ws.on('_send', function(data) {
+      //console.log("RECEIVE", data);
+      client.receiveMessage(JSON.parse(data));
+    });
     var binStream = realtime.connection(ws, 'lol');
     binStream.connections.length.should.eql(1);
     binStream.stream.onEnd(function() {
+      //console.log("END STREAM");
       binStream.connections.length.should.eql(0);
-      ws._sent.should.eql([{
-        cmd: 'setGames',
-        args: [[]]
+      withoutGuids(client.games).should.eql([{
+        title: 'sup',
+        description: 'yo',
+        url: 'http://example.org/'
       }]);
       done();
     });
-    ws.emit('close');
+    client.addGame({
+      title: 'sup',
+      description: 'yo',
+      url: 'http://example.org/'
+    });
+    process.nextTick(function() {
+      ws.emit('close');
+    });
   });
 });
